78fc27279d0cd8ce47fd112b05add0fb0f9f3142
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-23 03:17:11 +0600

    types

---
+++ types.js
@@ -0,0 +1,17 @@
+// String Type Variable
+var name = 'arif hossain';
+var address = '60ft, Mirpur, Dhaka';
+var country = "Bangladesh"
+// console.log (address);
+
+// Boolean Type
+var isRich = true;
+// console.log(isRich);
+var isPassed = false;
+// console.log(isPassed);
+// console.log(typeof isPassed);
+// console.log(typeof country);
+
+// Number Type / Numeric Type
+var price = 555;
+console.log (typeof price);
