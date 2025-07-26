9f1a321823dc0eff9087f894ef5b814549b3eb57
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-27 02:10:59 +0600

    number.js

---
+++ number.js
@@ -0,0 +1,7 @@
+var orangePrice = 22.5;
+var chocolate = 0.5;
+var applePrice = parseInt('20');
+// console.log (typeof orangePrice);
+// console.log (typeof applePrice);
+// console.log (orangePrice + applePrice);
+console.log(chocolate);
