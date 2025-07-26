f66681512850901bc9beacebda635a87ee2a26b5
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-27 02:53:54 +0600

    fruits-price

---
+++ fruits-price.js
@@ -0,0 +1,8 @@
+var moneyInPocket = 1000;
+var applePrice = 100;
+var orangePrice = 200;
+var totalFruitPrice = applePrice + orangePrice;
+var moneyPay = moneyInPocket - totalFruitPrice;
+var moneyReturn = moneyPay;
+console.log (moneyReturn);
+
