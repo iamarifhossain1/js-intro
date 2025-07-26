3a264ed9131eb5a15209f183779dacddd318d748
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-27 02:36:21 +0600

    simple-math

---
+++ simple-math.js
@@ -0,0 +1,19 @@
+// var money = 10;
+// var money1 = 20;
+// var total = money + money1;
+// console.log (total);
+
+// var money = 20;
+// var money1 = 50;
+// var remainingMoney = money1 - money;
+// console.log (remainingMoney);
+
+// var money = 10;
+// var money1 = 20;
+// var multi = money * money1;
+// console.log (multi);
+
+var money = 10;
+var money1 = 20;
+var total = money1 / money;
+console.log (total);
