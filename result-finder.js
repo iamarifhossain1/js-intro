b4b66d398ce45fb2eecddfa4fdefd42ea2b0a2f5
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-27 03:04:11 +0600

    result-finder.js

---
+++ result-finder.js
@@ -0,0 +1,9 @@
+var mathematics = parseFloat(75.25);
+var biology = 65;
+var chemistry = 80;
+var physics = parseFloat(35.45);
+var bangla = parseFloat (99.50);
+
+ var result = mathematics + biology + chemistry + physics + bangla;
+ var totalAverage = result / 5
+ console.log (totalAverage.toFixed(2));
