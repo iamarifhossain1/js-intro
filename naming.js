4b3418d17d4e8aecfb02dddb2961b1b668299435
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-27 01:38:14 +0600

    naming.js

---
+++ naming.js
@@ -0,0 +1,26 @@
+// 1. No keyword in variable name
+var false = 45;
+var var = 50;
+
+// 2. No space or gap in variable name
+var is happy = false;
+var isHappy = true;
+
+// 3. No quote 
+var "address" = "kochu khet";
+
+// 4. Cannot start with a number but number other than the first letter is allowed
+var money = 45;
+var money2 = 55;
+var 3money = 88;
+
+// 5. variable name is case sensitive
+var address
+var Address 
+var ADDRESS 
+
+// 6. How to write a long variable name
+var my current home address = "mirpur 60ft";
+var myCurrentHomeAddress = "mirpur 60ft"; // Camel Case
+var my_current_home_address = "mirpur 60ft"; // Snake Case
+var MyCurrentHomeAddress = "mirpur 60ft"; // pascal Case
